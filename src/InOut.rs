01186652cade697c7f5ab5584a07504a6b9ac619
Vamshi Krishna S <107762231+VamshiKrsna@users.noreply.github.com>
2024-12-26 20:44:44 +0530

    restart

--- src/InOut.rs
+++ src/InOut.rs
@@ -1,3 +1,12 @@
+// use std::io::Write;
+
 fn main(){
     println!("Hello World");
+    let mut line = String::new();
+    println!("Enter Your Name : ");
+    let b1 = std::io::stdin().read_line(&mut line).unwrap();
+    println!("Hello, {}", line);
+    println!("Change the Name : ");
+    let b2 = std::io::stdin().read_line(&mut line).unwrap();
+    println!("Changed Name to : {}",line);
 }
